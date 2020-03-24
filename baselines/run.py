0a54b6d1992b0f8def428ce599826b807164d7dc
ekwyss <ekwyss@gmail.com>
2020-03-24 11:06:11 -0400

    add env_kwargs

--- baselines/run.py
+++ baselines/run.py
@@ -110,7 +110,7 @@ def build_env(args):
         get_session(config=config)
 
         flatten_dict_observations = alg not in {'her'}
-        env = make_vec_env(env_id, env_type, args.num_env or 1, seed, reward_scale=args.reward_scale, flatten_dict_observations=flatten_dict_observations)
+        env = make_vec_env(env_id, env_type, args.num_env or 1, seed, reward_scale=args.reward_scale, flatten_dict_observations=flatten_dict_observations, env_kwargs = {"num_goals" : 1, "subgoal_rewards" : [0]})
 
         if env_type == 'mujoco':
             env = VecNormalize(env, use_tf=True)
